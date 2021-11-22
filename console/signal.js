dd06dc2ad9daa64835c5d0c076c4bd3b0b52d24e
qwertypnk <qwertypnk@protonmail.com>
2021-11-22 08:28:39 +0200

    deploy: 1.0.4

--- console/signal.js
+++ console/signal.js
@@ -1,16 +1,20 @@
 #!/usr/bin/env node
 
-const { SignalContract } = require('@sotaoi/omni/contracts/signal-contract');
+const { SignalContract } = require('@sotaoi/signal');
 const { execSync } = require('child_process');
 const path = require('path');
 
 const main = async () => {
+  const signalCore = require('./core');
+
+  await signalCore.integrity();
+
   class Signal extends SignalContract {
     //
   }
 
   new Signal(require('../package.json'), {
-    ...require('./core'),
+    ...signalCore,
   })
     .console()
     .command('clean:bootstrap', null, null, () => {
@@ -21,6 +25,11 @@ const main = async () => {
     })
     .command('deploy:somni', null, null, () => {
       execSync(`node ./console/commands/deploy/deploy-somni`, { cwd: path.resolve('./'), stdio: 'inherit' });
+    })
+    .command('publish:somni', null, null, () => {
+      execSync(`node ./console/commands/deploy/deploy-somni`, { cwd: path.resolve('./'), stdio: 'inherit' });
+      execSync(`npm publish --access public`, { cwd: path.resolve('./deployment'), stdio: 'inherit' });
+      execSync(`node ./console/commands/clean/clean-somni`, { cwd: path.resolve('./'), stdio: 'inherit' });
     })
     .run();
 };
